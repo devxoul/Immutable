c04f66ee457c282f18fb262bb96918af59565dc4
Suyeol Jeon <devxoul@gmail.com>
2017-09-21 22:02:51 +0900

    Migrate to Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Immutable"
+  name: "Immutable",
+  products: [
+    .library(name: "Immutable", targets: ["Immutable"]),
+  ],
+  targets: [
+    .target(name: "Immutable"),
+    .testTarget(name: "ImmutableTests", dependencies: ["Immutable"]),
+  ]
 )
