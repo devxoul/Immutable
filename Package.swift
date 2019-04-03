a7e49d87704181651635cc1aa3ec642b85d67179
Suyeol Jeon <devxoul@gmail.com>
2019-04-03 21:27:14 +0900

    Use Swift 5 in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "Immutable",
+  platforms: [
+    .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
+  ],
   products: [
     .library(name: "Immutable", targets: ["Immutable"]),
   ],
   targets: [
     .target(name: "Immutable"),
     .testTarget(name: "ImmutableTests", dependencies: ["Immutable"]),
-  ]
+  ],
+  swiftLanguageVersions: [.v5]
 )
